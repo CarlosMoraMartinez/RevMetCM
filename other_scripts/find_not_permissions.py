b5b35fa0776e665c7b7133137c972628e853f0eb
Carlos Mora <carmoma9@gmail.com>
2022-11-14 22:09:45 +0100

    find_not_permissions.py

--- other_scripts/find_not_permissions.py
+++ other_scripts/find_not_permissions.py
@@ -15,7 +15,7 @@ def main():
     fnames = glob.glob(INDIR)
     samples = set([os.path.basename(i).split(".")[0] for i in fnames])
     samples = {s:[f for f in fnames if os.path.basename(f).startswith(s + '.')] for s in samples}
-    for k, v in samples.values():
+    for k, v in samples.items():
         copy2outdir(k, v)
       
 if __name__ == "__main__":
