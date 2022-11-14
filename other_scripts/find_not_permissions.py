b72ea1411ff57cd7c2bf93326995d0c66cd88166
Carlos Mora <carmoma9@gmail.com>
2022-11-14 22:08:32 +0100

    find_not_permissions.py

---
+++ other_scripts/find_not_permissions.py
@@ -0,0 +1,22 @@
+import os
+import glob
+
+INDIR = "/scratch/groups/assembly/shared/projects/pollergen/data/illumina_genome_skim_EASI_48/*/*gz"
+
+def copy2outdir(sample, files):
+    for f in files:
+        try:
+            cmd = f"zcat {f} | head -1 > err.txt"
+            os.system(cmd)
+        except:
+            print(f"{sample}\t{f}") 
+    
+def main():
+    fnames = glob.glob(INDIR)
+    samples = set([os.path.basename(i).split(".")[0] for i in fnames])
+    samples = {s:[f for f in fnames if os.path.basename(f).startswith(s + '.')] for s in samples}
+    for k, v in samples.values():
+        copy2outdir(k, v)
+      
+if __name__ == "__main__":
+    main()
